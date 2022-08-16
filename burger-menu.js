8c6bba42243a8961569ce10405ae94386dc05acd
Dmitry Prigulnov <prigulnov@mail.ru>
2022-08-16 18:19:56 +0500

    feat: реализованы header, раздел с метизами и сваркой

---
+++ burger-menu.js
@@ -0,0 +1,32 @@
+function getBurgerMenu() {
+  const burgerElement = document.getElementsByClassName(
+    'header__nav__gamburger-container'
+  )[0];
+  document.body.addEventListener('click', (e) =>
+    hendlerClick(e.target, burgerElement)
+  );
+}
+
+function hendlerClick(elementWithEvent, burgerElement) {
+  const headerNavListElement =
+    document.getElementsByClassName('header__nav__list')[0];
+  const header__nav__itemElements =
+    document.getElementsByClassName('header__nav__item');
+  const mainElement = document.querySelector('main');
+
+  if (elementWithEvent === burgerElement) {
+    for (let item of Array.from(header__nav__itemElements)) {
+      item.classList.toggle('header__nav__item-visible');
+    }
+
+    mainElement.classList.toggle('main-longer');
+  } else if (headerNavListElement) {
+    mainElement.classList.remove('main-longer');
+
+    for (let item of Array.from(header__nav__itemElements)) {
+      item.classList.remove('header__nav__item-visible');
+    }
+  }
+}
+
+getBurgerMenu();
