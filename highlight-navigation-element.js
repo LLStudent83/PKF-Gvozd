bf151507d7f0c650f2f9b2f4f7f0785ba4f75f84
Dmitry Prigulnov <prigulnov@mail.ru>
2022-08-17 15:21:36 +0500

    fix: проблема с позиционированием бургера, подсветка активного пункта меню, вылет пункта меню за бордер

--- highlight-navigation-element.js
+++ highlight-navigation-element.js
@@ -2,12 +2,20 @@ function highlight_navigation_element() {
   const arr_nav_items = Array.from(
     document.querySelectorAll('.header__nav__item a')
   );
-  const url = document.location.href;
-
   for (let item of arr_nav_items) {
-    if (url === item.href.slice(0, item.href.length - 1)) {
-      //slice(0, item.href.length-1) убирает # на конце item.href
-      item.classList.add('header__nav__item-active');
+    item.addEventListener("click", (e)=> handler(e.target))
+  }
+}
+
+function handler(element) {
+  const nav_item_active = 
+    document.querySelector('.header__nav__item-active');
+  if (!nav_item_active) {
+    element.classList.add("header__nav__item-active");
+  } else {
+    if (element !== nav_item_active) {
+      nav_item_active.classList.remove("header__nav__item-active");
+      element.classList.add("header__nav__item-active");
     }
   }
 }
